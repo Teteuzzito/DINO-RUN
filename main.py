64853259ce8ad6e166fe2648f0c354bcf0b704a5
HeitorSantana <151783683+HeitorSantana@users.noreply.github.com>
2025-07-14 22:10:18 -0300

    tela de menu

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# main.py
+
 import pygame
 import sys
 from config import *
@@ -10,12 +10,26 @@ pygame.display.set_caption(TITULO)
 relogio = pygame.time.Clock()
 rodando = True
 
+
+def menu_principal():
+    while True:
+        janela.fill((0, 0, 0))
+        pygame.display.update()
+        for evento in pygame.event.get():
+            if evento.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if evento.type == pygame.KEYDOWN:
+                if evento.key == pygame.K_SPACE:
+                    return
+menu_principal()
+
 while rodando:
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
             rodando = False
 
-    janela.fill((255, 255, 255))  # Fundo branco temporário
+    janela.fill((255, 255, 255))  
     pygame.display.update()
     relogio.tick(FPS)
 
