2133fbfe454cb4e0c01065cc675306279fb3f67c
HeitorSantana <151783683+HeitorSantana@users.noreply.github.com>
2025-07-14 22:08:39 -0300

    estrutura base

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-print('Hello World!')
+# main.py
+import pygame
+import sys
+from config import *
+
+pygame.init()
+janela = pygame.display.set_mode((LARGURA, ALTURA))
+pygame.display.set_caption(TITULO)
+
+relogio = pygame.time.Clock()
+rodando = True
+
+while rodando:
+    for evento in pygame.event.get():
+        if evento.type == pygame.QUIT:
+            rodando = False
+
+    janela.fill((255, 255, 255))  # Fundo branco temporário
+    pygame.display.update()
+    relogio.tick(FPS)
+
+pygame.quit()
+sys.exit()
+
