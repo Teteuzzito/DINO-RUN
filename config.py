9fe0541962cad08703657d2ad89784112cb1c3de
Teteuzzito <matheus.jbr12345@gmail.com>
2025-07-14 22:27:05 -0300

    Adicionei algumas configurações no config

--- config.py
+++ config.py
@@ -1,5 +1,22 @@
-
+#TELA
 LARGURA = 800
 ALTURA = 600
 FPS = 60
 TITULO = "Dino Run"
+
+#CORES
+branco = (255, 255, 255)
+preto = (0, 0, 0)
+verde = (0, 255, 0)
+cor_explosao = (255, 120, 0)
+
+#TEXTO
+nome_fonte = 'assets/fonte/Pixeled.ttf'
+tamanho_fonte = 24
+tamanho_fonte_grande = 48
+
+#JOGO
+velocidade_inicial_obstaculo = 200
+intervalo_spawn_obstaculo = 0.8
+tamanho_dino = (50, 50)
+pontuacao_maxima = 999_999
